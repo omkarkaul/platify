b2642d264c0f0b0cb14d6f2fdb9d83b388355b63
Omkar Kaul <omkar@xtra.co.nz>
2020-04-23 20:04:35 +1200

    init with file server and landing page built

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname+'/assets')); // providing static assets to web server
+
+app.get('/', (req, res) => {
+    console.log('serving root');
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/search', (req, res) => {
+    console.log('serving search');
+    res.sendFile(path.join(__dirname+'/pages/search.html'));
+});
+
+app.get('/about', (req, res) => {
+    console.log('serving about');
+    res.sendFile(path.join(__dirname+'/pages/search.html'));
+});
+
+app.listen(3000, () => {
+    console.log('listening on 3000')
+});
