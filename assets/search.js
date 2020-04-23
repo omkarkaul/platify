c45f4347ec9b9b34d59f84da4e0e7214c684ea75
Omkar Kaul <omkar@xtra.co.nz>
2020-04-24 00:42:10 +1200

    updated searchElement handler and added updateDOM function to update availability

--- assets/search.js
+++ assets/search.js
@@ -1,16 +1,27 @@
 async function getAllData(plate) {
-    const res = await fetch(`http://localhost:8080/plate`);
+    
+    const res = await fetch(`http://localhost:8080/${plate}`);
     const json = await res.json();
 
     updateDOM(json);
 };
 
 function updateDOM(data) {
-    console.log(data);
+    // update plate-availability
+    const plateAvailability = document.getElementById('plate-availability');
+    plateAvailability.innerHTML = data.availability? `Availability: True!` : `Availability: False!`
 }
 
 const searchElement = document.getElementById('plate-search');
+const plateElement = document.getElementById('plate');
 
 searchElement.addEventListener('input', () => {
-    getAllData(searchElement.value);
-})
+    const plateValue = searchElement.value;
+
+    if (plateValue !== '') {
+        plateElement.innerHTML = searchElement.value;
+        getAllData(searchElement.value);
+    } else {
+        plateElement.innerHTML = 'N/A';
+    }
+});
