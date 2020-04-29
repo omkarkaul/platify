8089a7236319593b149fedee98242e09d327e104
Omkar Kaul <omkar@xtra.co.nz>
2020-04-29 14:48:24 +1200

    modified script so that it redirects to /search after some random number of milliseconds between 1000 and 3000

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,5 @@
-location.assign(`${location.origin}/search`);
+let randNum = Math.random() * (3000 - 1000) + 1000
+
+setTimeout(() => {
+    location.assign(`${location.origin}/search`);
+}, randNum);
